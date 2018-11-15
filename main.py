cae045631adce73ee197ccaa9e619bf42fa1b158
yuki <yuki.ohira.vine2017@gmail.com>
2018-11-15 17:38:41 +0900

    Add depths to the code to check suitable depth of the tree

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import time
 import numpy as np
 import pandas as pd
 import pickle
+
  
 
 from sklearn.tree import DecisionTreeClassifier
@@ -16,15 +17,16 @@ from sklearn import tree
 from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
+from matplotlib import pyplot as plt
 
 #from MakeReceivedImg import MakeReceivedImg
 from MakeDataset import MakeDataset
 
 
-sigma = 0.4
+sigma = 0.3
 noise = 0.5
-led_num = 16
-num_error = 0
+led_num = 9
+
 sqrt_len = int(np.sqrt(led_num))
 filename = 'finalized_model.sav'
 
@@ -56,29 +58,56 @@ def DataFrameTest(loopTimes=None, numLEDs=None, xTarget=None, yTarget=None, nois
 
 
 print('============== Start Training ==============')
+
+# 深度設定: 1からmaxdepthまで
+maxdepth = 2
+depths = range(1, maxdepth)
+
+# 決定木の深度ごとにBERを計算
+accuracy_scores = []
 start = time.time()
-for y in range(sqrt_len):
-    for x in range(sqrt_len):
-        df_train, df_train_target = DataFrameTrain(1000, led_num, y, x, noise)
-        df_test, df_test_target = DataFrameTest(100, led_num, y, x, noise)
-        
-        clf = tree.DecisionTreeClassifier(max_depth=4)  
-        clf.fit(df_train, df_train_target)
-        
-        predicted = clf.predict(df_test)
-        num_error += (sum(predicted != df_test_target))
+
+for depth in depths:
     
-    elapsed_time = time.time() - start
+    num_error = 0
     
-    print('Training progress.....')
-    print('現在: ', ((y + 1) * 100) / sqrt_len, '% です. (合計処理時間:', elapsed_time, '[sec])')
+    for y in range(sqrt_len):
+        for x in range(sqrt_len):
+            df_train, df_train_target = DataFrameTrain(512, led_num, y, x, noise)
+            df_test, df_test_target = DataFrameTest(1000, led_num, y, x, noise)
+            
+            clf = tree.DecisionTreeClassifier(max_depth=depth)  
+            clf.fit(df_train, df_train_target)
+            
+            predicted = clf.predict(df_test)
+            num_error += (sum(predicted != df_test_target))
+        
+        elapsed_time = time.time() - start
+        
+        print('Training progress.....')
+        print('現在: ', ((y + 1) * 100) / (sqrt_len * maxdepth), '% です. (合計処理時間:', elapsed_time, '[sec])')
+    
+    score = num_error / (len(df_test) * led_num)
+    accuracy_scores.append(score)
+    print("max depth={0}: {1}".format(depth, score))
+    
     
 # save trained model
-pickle.dump(clf, open(filename, 'wb'))
+# pickle.dump(clf, open(filename, 'wb'))
+
 
 print('Complete Training')
+
+# 最大深度ごとの正解率を折れ線で可視化
+
+deep = list(depths)
+plt.plot(deep, accuracy_scores)
+
+plt.xlabel('max depth')
+plt.ylabel('BER')
+#plt.show()
 #print('=============== predicted ===============')
 #print(predicted)
-print('============== correct_ans ==============')
-print('BER:', num_error / (len(df_test) * led_num))
-print('学習したモデルを', filename, 'に保存しました')
+#print('============== correct_ans ==============')
+#print('BER:', num_error / (len(df_test) * led_num))
+#print('学習したモデルを', filename, 'に保存しました')
